3b3e292de148687892351e6de1ff65bb9776a9e2
abesadzealex2@gmail.com <abesadzealex2@gmail.com>
2025-10-02 18:04:59 +0400

    commit

---
+++ python course/day 0/Classwork/lesson.py
@@ -0,0 +1,14 @@
+name="alex"
+age=11
+print(age,name)
+
+
+
+
+name="alex" 
+number=4
+print(name)
+print(number)
+
+str("hello")
+print(str)
---
+++ python course/day 01/lesson.py
@@ -0,0 +1,25 @@
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+
+
+sawmeli="xinkali,goris xorci,limoni,mwvadi,awaruli xawapuri"
+print(sawmeli)
+
+
+("hello alex abesadze") 
+
+
+#########
+# hello #
+#########
+
+
+
+xinkali="13 lari"
+limoni="50 tetri"
+xorci="28 lari"
+xawapuri="15 lari"
+mwvadi="20 lari"
